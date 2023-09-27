268f44f45a64436da77467f017ad8e792adfaa98
chandrabhushan-kumar <kumar30895@gmail.com>
2023-09-28 01:52:43 +0530

    fixing bug

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
 import StockPriceTracker from './StockPriceTracker';
-import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
       <StockPriceTracker/>
     </div>
   );
